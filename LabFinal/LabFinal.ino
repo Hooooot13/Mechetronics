25eb12712e4496cafd22f85e04aa358a5597c304
Hooooot13 <lcdx5000@gmail.com>
2020-04-20 15:01:21 -0500

    commit

--- LabFinal/LabFinal.ino
+++ LabFinal/LabFinal.ino
@@ -1,6 +1,24 @@
 /*
-
-
+Proposal for a Music Box Like device with two separately timed beats and Pezo Electric
+Toners. We will use the various analog input to set the beat and tone of the devices.
+- 2 sensors
+  - Encoder knob
+  - PWM
+  - Timer counter
+  - (Optional) Joystick
+- 2 actuators
+  - 2 motors
+  - PWM
+  - HMI
+  - LCD
+  - (Optional) 8x8 LED display
+- Other Inputs
+  - 4 buttons
+- Required functionalities
+  - ADC
+  - Timer counter
+  - PWM
+  - LCD display
 */
 
 
